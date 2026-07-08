ca92a7ea829931b1c32434d28697a2fb5af893f0
willdeng677-pixel <willdeng677@gmail.com>
2026-07-08 12:36:37 +0000

    C program which print the string exactly as it was given

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-	printf("With proper grammer, but the outcome is a piece of art\n");
+	printf("With proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
