e822b6e37f854ceaceafe19ffb999341ddd92018
willdeng677-pixel <willdeng677@gmail.com>
2026-07-09 23:37:51 +0000

    main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,13 @@
-main h
-#ifndef MAIN_H
-#define MAIN_H
+#include <unistd.h>
 
-int _putchar(char c);
-
-#endif
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
