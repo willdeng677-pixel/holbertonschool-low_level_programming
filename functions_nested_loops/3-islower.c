453c9d55ba9ec2100ddfe23fe12b9a43e8dbe6d6
willdeng677-pixel <willdeng677@gmail.com>
2026-07-10 01:14:40 +0000

    Program that check wether the letter is lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase character
+ * @c: Character to check
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+
+return (0);
+}
