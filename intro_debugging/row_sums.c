6fd17b2b699ba46450db98b5bc432dd7363211e0
willdenng677-pixel <willdeng677@gmail.com>
2026-07-10 07:20:27 +0000

    program to sum up rows

---
+++ intro_debugging/row_sums.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int row_sum(int row, int cols)
+{
+int c;
+int sum = 0;
+
+for (c = 1; c <= cols; c++)
+sum += row * c;
+
+return (sum);
+}
+
+int total_sum(int rows, int cols)
+{
+int r;
+int total = 0;
+
+for (r = 1; r <= rows; r++)
+{
+total += row_sum(r, cols);
+}
+
+return (total);
+}
+
+/* -- DO NOT Modify the code below this line -- */
+
+int main(void)
+{
+printf("%d\n", total_sum(3, 3));
+return (0);
+}
