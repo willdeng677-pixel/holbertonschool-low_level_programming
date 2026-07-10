5a7960085523230db5e5c4f5820f1730a4146aeb
willdeng677-pixel <willdeng677@gmail.com>
2026-07-10 02:28:41 +0000

    program that print numbers upp to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: Starting number
+ */
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+while (n < 98)
+{
+printf("%d, ", n);
+n++;
+}
+}
+else
+{
+while (n > 98)
+{
+printf("%d, ", n);
+n--;
+}
+}
+
+printf("%d\n", 98);
+}
