0dc43876f8b85d2f04fdbf946668e50a8f8a2458
willdeng677-pixel <willdeng677@gmail.com>
2026-07-10 00:22:44 +0000

    program that print alphabet in lowercase

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+__putchar(c);
+}
+
+__putchar('\n');
+}
