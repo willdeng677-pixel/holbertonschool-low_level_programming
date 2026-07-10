f8da7a4b11e50246fbd26dd9468f5b277613fc44
willdeng677-pixel <willdeng677@gmail.com>
2026-07-10 12:34:25 +0000

    program which print straight line and \n if n = 0

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_line - Draws a straight line in the terminal.
+* @n: Number of underscores to print.
+*
+* Return: Nothing.
+*/
+void print_line(int n)
+{
+int i;
+
+if (n <= 0)
+{
+_putchar('\n');
+return;
+}
+
+for (i = 0; i < n; i++)
+_putchar('_');
+
+_putchar('\n');
+}
