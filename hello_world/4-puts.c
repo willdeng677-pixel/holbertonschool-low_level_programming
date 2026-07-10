06137f5d6960bb284c6baee25849c913f172051a
willdenng677-pixel <willdeng677@gmail.com>
2026-07-10 08:19:04 +0000

    program that print sting lines

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
-*Program that pring string
-*return 0 if successful
-*return i if not successful
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
