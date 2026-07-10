e56e6b9691cb6afa8e593e04fb94b0f37339aa97
willdeng677-pixel <willdeng677@gmail.com>
2026-07-10 12:00:35 +0000

    program which print numbers from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_numbers - Prints the numbers from 0 to 9.
+*
+* Return: Nothing.
+*
+*/
+void print_numbers(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+_putchar(n + '0');
+}
+
+_putchar('\n');
+} 
