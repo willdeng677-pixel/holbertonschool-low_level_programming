8a8762c92ee380283a93cd27f1a5b080adb01a85
willdeng677-pixel <willdeng677@gmail.com>
2026-07-10 02:53:24 +0000

    program that print single digit number of base 10

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Prints all single digit numbers of base 10.
- *
- * Return: Always 0.
- */
+* main - Prints all single digit numbers of base 10.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-    int n;
+int n;
 
-    for (n = 0; n < 10; n++)
-    {
-        putchar(n + '0');
-    }
+for (n = 0; n < 10; n++)
+{
+putchar(n + '0');
+}
 
-    putchar('\n');
+putchar('\n');
 
-    return (0);
+return (0);
 }
