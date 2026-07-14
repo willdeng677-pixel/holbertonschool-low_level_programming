a15430277db625d53640362c7c58eaab509ce333
willdeng677-pixel <willdeng677@gmail.com>
2026-07-14 10:27:22 +0000

    program that copies string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies a string.
+ * @dest: The destination buffer.
+ * @src: The source string.
+ * @n: The maximum number of characters to copy.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
