6e4be838354315e46ce6c605ea573daf31237ed9
willdeng677-pixel <willdeng677@gmail.com>
2026-07-14 10:12:55 +0000

    program that concatenates two string

---
+++ pointers_arrays_strings/1-strcat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates two strings using at most n bytes from src.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The maximum number of bytes to use from src.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	/* Find the end of dest */
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	/* Copy at most n characters from src */
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	/* Add terminating null byte */
+	dest[i] = '\0';
+
+	return (dest);
+}
