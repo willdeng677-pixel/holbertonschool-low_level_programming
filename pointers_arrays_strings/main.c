f23a215015b59dd5419681c28c35e5d1e0520f4a
willdeng677-pixel <willdeng677@gmail.com>
2026-07-14 03:36:13 +0000

    reset_to_98 which update pointer value

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,6 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <stdio.h>
+#include "main.h"
 
-void reset_to_98(int *n);
+int main(void)
+{
+int x = 402;
 
-#endif
+printf("Before: %d\n", x);
+
+reset_to_98(&x);
+
+printf("After: %d\n", x);
+
+return (0);
+}
