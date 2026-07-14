fc6aa298844eef30e78456949e458b81f7751bc3
willdeng677-pixel <willdeng677@gmail.com>
2026-07-14 12:45:43 +0000

    program that run multi-phase

---
+++ benchmarking/instrumentation_lab.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <time.h>
+
+#define DATASET_SIZE 50000
+#define SEED_VALUE 42u
+
+static int dataset[DATASET_SIZE];
+
+static unsigned int next_value(unsigned int *state)
+{
+    *state = (*state * 1103515245u) + 12345u;
+    return *state;
+}
+
+static void build_dataset(void)
+{
+    unsigned int state;
+    int i;
+
+    state = SEED_VALUE;
+
+    for (i = 0; i < DATASET_SIZE; i++)
+        dataset[i] = (int)(next_value(&state) % 100000);
+}
+
+static void process_dataset(void)
+{
+    int i;
+    int v;
+
+    for (i = 0; i < DATASET_SIZE; i++)
+    {
+        v = dataset[i];
+        v = (v * 3) + (v / 7) - (v % 11);
+        if (v < 0)
+            v = -v;
+        dataset[i] = v;
+    }
+}
+
+static unsigned long reduce_checksum(void)
+{
+    unsigned long sum;
+    int i;
+
+    sum = 0;
+    for (i = 0; i < DATASET_SIZE; i++)
+        sum = (sum * 131ul) + (unsigned long)dataset[i];
+
+    return sum;
+}
+
+int main(void)
+{
+    unsigned long checksum;
+
+    /* Students must add clock-based timing and print required lines. */
+
+    build_dataset();
+    process_dataset();
+    checksum = reduce_checksum();
+
+    if (checksum == 0ul)
+        printf("impossible\n");
+
+    /* Required output (exact format, no extra lines):
+     * TOTAL seconds: <float>
+     * BUILD_DATA seconds: <float>
+     * PROCESS seconds: <float>
+     * REDUCE seconds: <float>
+     */
+
+    return 0;
+}
