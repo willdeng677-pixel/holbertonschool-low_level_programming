f60d9b537fe048aad87a24b420e960542cefc864
willdeng677-pixel <willdeng677@gmail.com>
2026-07-14 03:49:32 +0000

    reset_to_98 which update pointer value

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * reset_to_98 - updates the value pointed to by n to 98
+ * @n: pointer to an integer
+ */
+
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
+
 int main(void)
 {
-int x = 402;
+int n = 402;
 
-printf("Before: %d\n", x);
+printf("Before: %d\n", n);
 
-reset_to_98(&x);
+reset_to_98(&n);
 
-printf("After: %d\n", x);
+printf("After: %d\n", n);
 
 return (0);
 }
