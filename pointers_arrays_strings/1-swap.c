f5528cdf5b2566468397d51c3362e783b6419729
willdeng677-pixel <willdeng677@gmail.com>
2026-07-14 05:47:30 +0000

    Function that swap values

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,5 +1,10 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ */
 void swap_int(int *a, int *b)
 {
 int temp;
@@ -7,22 +12,4 @@ int temp;
 temp = *a;
 *a = *b;
 *b = temp;
-} 
-
-void swap_int(int *a, int *b);
-
-int main(void)
-{
-int x = 10;
-int y = 20;
-
-printf("Before: x = %d, y = %d\n", x, y);
-
-swap_int(&x, &y);
-
-printf("After: x = %d, y = %d\n", x, y);
-
-return (0);
 }
-
-
