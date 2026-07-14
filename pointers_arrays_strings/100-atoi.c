bfb9d156239249c7e3ba6e3a3daa6604b1eaf6e4
willdeng677-pixel <willdeng677@gmail.com>
2026-07-14 07:04:16 +0000

    program that covert string to an integer

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: String to convert.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+int i;
+int sign;
+unsigned int num;
+
+i = 0;
+sign = 1;
+num = 0;
+
+while (s[i] != '\0')
+{
+if (s[i] == '-')
+sign *= -1;
+
+if (s[i] >= '0' && s[i] <= '9')
+{
+while (s[i] >= '0' && s[i] <= '9')
+{
+num = (num * 10) + (s[i] - '0');
+i++;
+}
+
+return (num * sign);
+}
+
+i++;
+}
+
+return (0);
+}
