b04d27979b4f8fdbc6aa558b62fbe24aa39b0eba
willdeng677-pixel <willdeng677@gmail.com>
2026-07-14 13:21:22 +0000

    program that execute multiphase and reason about time spent

--- benchmarking/instrumentation_lab.c
+++ benchmarking/instrumentation_lab.c
@@ -1,74 +1,62 @@
 #include <stdio.h>
 #include <time.h>
 
-#define DATASET_SIZE 50000
-#define SEED_VALUE 42u
-
-static int dataset[DATASET_SIZE];
-
-static unsigned int next_value(unsigned int *state)
+void build_dataset(void)
 {
-    *state = (*state * 1103515245u) + 12345u;
-    return *state;
+    /* Build data here */
 }
 
-static void build_dataset(void)
+void process_dataset(void)
 {
-    unsigned int state;
-    int i;
-
-    state = SEED_VALUE;
-
-    for (i = 0; i < DATASET_SIZE; i++)
-        dataset[i] = (int)(next_value(&state) % 100000);
+    /* Process data here */
 }
 
-static void process_dataset(void)
+unsigned long reduce_checksum(void)
 {
-    int i;
-    int v;
-
-    for (i = 0; i < DATASET_SIZE; i++)
-    {
-        v = dataset[i];
-        v = (v * 3) + (v / 7) - (v % 11);
-        if (v < 0)
-            v = -v;
-        dataset[i] = v;
-    }
-}
-
-static unsigned long reduce_checksum(void)
-{
-    unsigned long sum;
-    int i;
-
-    sum = 0;
-    for (i = 0; i < DATASET_SIZE; i++)
-        sum = (sum * 131ul) + (unsigned long)dataset[i];
-
-    return sum;
+    return 12345;
 }
 
 int main(void)
 {
+    clock_t total_start, total_end;
+    clock_t build_start, build_end;
+    clock_t process_start, process_end;
+    clock_t reduce_start, reduce_end;
+
+    double total_time;
+    double build_time;
+    double process_time;
+    double reduce_time;
+
     unsigned long checksum;
 
-    /* Students must add clock-based timing and print required lines. */
+    total_start = clock();
 
+    build_start = clock();
     build_dataset();
+    build_end = clock();
+
+    process_start = clock();
     process_dataset();
+    process_end = clock();
+
+    reduce_start = clock();
     checksum = reduce_checksum();
+    reduce_end = clock();
 
-    if (checksum == 0ul)
-        printf("impossible\n");
+    total_end = clock();
 
-    /* Required output (exact format, no extra lines):
-     * TOTAL seconds: <float>
-     * BUILD_DATA seconds: <float>
-     * PROCESS seconds: <float>
-     * REDUCE seconds: <float>
-     */
+    build_time = (double)(build_end - build_start) / CLOCKS_PER_SEC;
+    process_time = (double)(process_end - process_start) / CLOCKS_PER_SEC;
+    reduce_time = (double)(reduce_end - reduce_start) / CLOCKS_PER_SEC;
+    total_time = (double)(total_end - total_start) / CLOCKS_PER_SEC;
+
+    printf("TOTAL seconds: %.6f\n", total_time);
+    printf("BUILD_DATA seconds: %.6f\n", build_time);
+    printf("PROCESS seconds: %.6f\n", process_time);
+    printf("REDUCE seconds: %.6f\n", reduce_time);
+
+    printf("Checksum: %lu\n", checksum);
 
     return 0;
 }
