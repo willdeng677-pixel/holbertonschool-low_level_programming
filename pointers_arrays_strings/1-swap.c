0a4322da32cf010db880fae8a2e59dc05d2eee5a
willdeng677-pixel <willdeng677@gmail.com>
2026-07-14 04:05:14 +0000

    Program that swap variables values

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,28 @@
+#include <stio.h>
+
+void swap_int(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+} 
+
+void swap_int(int *a, int *b);
+
+int main(void)
+{
+int x = 10;
+int y = 20;
+
+printf("Before: x = %d, y = %d\n", x, y);
+
+swap_int(&x, &y);
+
+printf("After: x = %d, y = %d\n", x, y);
+
+return (0);
+}
+
+
