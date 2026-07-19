3470c598bb213f33a32ed3d3a83fa706278b2102
willdeng677-pixel <willdeng677@gmail.com>
2026-07-19 07:03:29 +0000

    main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,7 +21,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
+void simple_print_buffer(char *buffer, unsigned int size);
 
 #endif
