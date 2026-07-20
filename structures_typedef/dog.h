8a855dfb574f209dffc4acd945eaebd8a70306ff
willdeng677-pixel <willdeng677@gmail.com>
2026-07-20 12:14:43 +0000

    Struct implementation

---
+++ structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+int main(void)
+{
+struct dog my_dog;
+
+my_dog.name = "Buddy";
+my_dog.age = 3.5;
+my_dog.owner = "William";
+
+printf("Name: %s\n", my_dog.name);
+printf("Age: %.1f\n", my_dog.age);
+printf("Owner: %s\n", my_dog.owner);
+
+return (0);
+}
