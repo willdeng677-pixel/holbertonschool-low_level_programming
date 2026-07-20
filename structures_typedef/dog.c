21d3813c54ca20358ba3ab4bd56ecc69731313b8
willdeng677-pixel <willdeng677@gmail.com>
2026-07-20 14:59:36 +0000

    Function that rename struct dog

---
+++ structures_typedef/dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+int main(void)
+{
+	dog_t my_dog;
+
+	my_dog.name = "Buddy";
+	my_dog.age = 3.5;
+	my_dog.owner = "William";
+
+	printf("Name: %s\n", my_dog.name);
+	printf("Age: %.1f\n", my_dog.age);
+	printf("Owner: %s\n", my_dog.owner);
+
+	return (0);
+}
