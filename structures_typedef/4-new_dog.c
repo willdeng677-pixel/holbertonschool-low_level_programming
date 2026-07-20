fb2b42e06294a2c88aad925022a39e1c0d4effd6
willdeng677-pixel <willdeng677@gmail.com>
2026-07-20 15:27:49 +0000

    Function that creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,32 +10,33 @@
  *
  * Return: pointer to new dog, or NULL if failed
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+dog_t *dog;
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
 
-	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
-	if (dog->name == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
+dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
 
-	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
-	if (dog->owner == NULL)
-	{
-		free(dog->name);
-		free(dog);
-		return (NULL);
-	}
+dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+if (dog->owner == NULL)
+{
+free(dog->name);
+free(dog);
+return (NULL);
+}
 
-	strcpy(dog->name, name);
-	strcpy(dog->owner, owner);
-	dog->age = age;
+strcpy(dog->name, name);
+strcpy(dog->owner, owner);
+dog->age = age;
 
-	return (dog);
+return (dog);
 }
