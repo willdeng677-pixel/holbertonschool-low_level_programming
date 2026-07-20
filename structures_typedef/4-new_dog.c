3bdd353f8ec9f6714a57059a17c2421a068b6897
willdeng677-pixel <willdeng677@gmail.com>
2026-07-20 15:32:30 +0000

    Function that creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,33 +10,47 @@
  *
  * Return: pointer to new dog, or NULL if failed
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *dog;
+	dog_t *dog;
+	unsigned int i;
 
-dog = malloc(sizeof(dog_t));
-if (dog == NULL)
-return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
 
-dog->name = malloc(sizeof(char) * (strlen(name) + 1));
-if (dog->name == NULL)
-{
-free(dog);
-return (NULL);
-}
+	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
 
-dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
-if (dog->owner == NULL)
-{
-free(dog->name);
-free(dog);
-return (NULL);
-}
+	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
 
-strcpy(dog->name, name);
-strcpy(dog->owner, owner);
-dog->age = age;
+	i = 0;
+	while (name[i] != '\0')
+	{
+		dog->name[i] = name[i];
+		i++;
+	}
+	dog->name[i] = '\0';
 
-return (dog);
+	i = 0;
+	while (owner[i] != '\0')
+	{
+		dog->owner[i] = owner[i];
+		i++;
+	}
+	dog->owner[i] = '\0';
+
+	dog->age = age;
+
+	return (dog);
 }
