feb69505c3efc4d96b43e18abc8b4cbdbb5f85aa
willdeng677-pixel <willdeng677@gmail.com>
2026-07-20 23:55:32 +0000

    Function that keep calling itself to solve problem

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts_recursion - prints a string followed by a new line
- * @s: pointer to the string
+ * @s: string to print
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		putchar('\n');
-		return;
-	}
+    if (*s == '\0')
+    {
+        _putchar('\n');
+        return;
+    }
 
-	putchar(*s);
-	_puts_recursion(s + 1);
+    _putchar(*s);
+    _puts_recursion(s + 1);
 }
