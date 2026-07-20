bcc063308af9065322a78c3f0ab70303cefcb32e
willdeng677-pixel <willdeng677@gmail.com>
2026-07-20 12:39:08 +0000

    Struct implementation in C language

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,9 +8,9 @@
  */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 /**
@@ -20,15 +20,15 @@ struct dog
  */
 int main(void)
 {
-	struct dog my_dog;
+struct dog my_dog;
 
-	my_dog.name = "Buddy";
-	my_dog.age = 3.5;
-	my_dog.owner = "William";
+my_dog.name = "Buddy";
+my_dog.age = 3.5;
+my_dog.owner = "William";
 
-	printf("Name: %s\n", my_dog.name);
-	printf("Age: %.1f\n", my_dog.age);
-	printf("Owner: %s\n", my_dog.owner);
+printf("Name: %s\n", my_dog.name);
+printf("Age: %.1f\n", my_dog.age);
+printf("Owner: %s\n", my_dog.owner);
 
-	return (0);
+return (0);
 }
