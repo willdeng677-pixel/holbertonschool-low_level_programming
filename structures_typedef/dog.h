bf784eb9829d23b58748f2761b1a32316e2ffa2b
willdeng677-pixel <willdeng677@gmail.com>
2026-07-20 12:21:25 +0000

    Struct implementation

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,23 +1,34 @@
 #include <stdio.h>
 
+/**
+ * struct dog - Defines a dog
+ * @name: Pointer to the dog's name
+ * @age: The dog's age
+ * @owner: Pointer to the owner's name
+ */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-struct dog my_dog;
+	struct dog my_dog;
 
-my_dog.name = "Buddy";
-my_dog.age = 3.5;
-my_dog.owner = "William";
+	my_dog.name = "Buddy";
+	my_dog.age = 3.5;
+	my_dog.owner = "William";
 
-printf("Name: %s\n", my_dog.name);
-printf("Age: %.1f\n", my_dog.age);
-printf("Owner: %s\n", my_dog.owner);
+	printf("Name: %s\n", my_dog.name);
+	printf("Age: %.1f\n", my_dog.age);
+	printf("Owner: %s\n", my_dog.owner);
 
-return (0);
+	return (0);
 }
