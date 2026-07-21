6d7d23e88643dd589f08eb9f0d5a1b00c607cfb2
willdeng677-pixel <willdeng677@gmail.com>
2026-07-21 13:10:58 +0000

    main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,16 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <sys/types.h>
+
+/**
+ * read_textfile - reads a text file and prints it to POSIX stdout
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: number of letters read and printed, or 0 on failure
+ */
 ssize_t read_textfile(const char *filename, size_t letters);
 
-#endif
+#endif /* MAIN_H */
