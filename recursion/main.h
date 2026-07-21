e2da447f15ca91ea3ea4b0acc5ecef4b46927d05
willdeng677-pixel <willdeng677@gmail.com>
2026-07-21 00:51:56 +0000

    main.h

--- recursion/main.h
+++ recursion/main.h
@@ -9,5 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_helper(int n, int guess);
+int is_prime_number(int n);
+int prime_helper(int n, int divisor);
 
 #endif
