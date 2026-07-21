8ab11d73a88b0ba56a8923034e81b363cc65b11f
willdeng677-pixel <willdeng677@gmail.com>
2026-07-21 13:14:21 +0000

    main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <unistd.h>
 #include <stddef.h>
 #include <sys/types.h>
 
@@ -12,5 +12,6 @@
  * Return: number of letters read and printed, or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif /* MAIN_H */
