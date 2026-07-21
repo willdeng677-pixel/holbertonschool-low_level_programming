5d1c80f0014cb26804166e50479c21bd54d54efe
willdeng677-pixel <willdeng677@gmail.com>
2026-07-21 13:49:43 +0000

    main.h

--- file_io/main.h
+++ file_io/main.h
@@ -17,5 +17,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_file(int fd_from, int fd_to, char *file_to);
+void close_file(int fd);
 
 #endif /* MAIN_H */
