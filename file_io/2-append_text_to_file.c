e22ec8cbb1932160012e6d7d5f2c515962883846
willdeng677-pixel <willdeng677@gmail.com>
2026-07-21 13:21:46 +0000

    main.h

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * append_text_to_file - appends text to the end of a file
+ * @filename: name of the file
+ * @text_content: text to append
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t len = 0;
+	ssize_t written;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+			len++;
+
+		written = write(fd, text_content, len);
+
+		if (written == -1 || written != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+
+	return (1);
+}
