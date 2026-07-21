9b36cb7e8b084584033e55159c2485c7c4342271
willdeng677-pixel <willdeng677@gmail.com>
2026-07-21 02:55:56 +0000

    Simple calculator

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -2,51 +2,107 @@
 
 int main(void)
 {
-int choice;
+int choice, check;
 float num1, num2, result;
 
-do
+for (;;)
 {
-printf("\n===== Simple Calculator =====\n");
-printf("1. Divide two numbers\n");
-printf("2. Exit\n");
+printf("\n===== SIMPLE CALCULATOR =====\n");
+printf("1. Addition\n");
+printf("2. Subtraction\n");
+printf("3. Multiplication\n");
+printf("4. Division\n");
+printf("0. Quit\n");
 printf("Enter your choice: ");
-scanf("%d", &choice);
+
+check = scanf("%d", &choice);
+
+if (check != 1)
+{
+printf("Invalid input! Please enter a number.\n");
+
+while (getchar() != '\n')
+;
+
+continue;
+}
+
+if (choice == 0)
+{
+printf("Goodbye!\n");
+break;
+}
+
+if (choice < 0 || choice > 4)
+{
+printf("Invalid menu choice!\n");
+continue;
+}
+
+/* Get first number */
+while (1)
+{
+printf("Enter first number: ");
+
+check = scanf("%f", &num1);
+
+if (check == 1)
+break;
+
+printf("Invalid input! Please enter a valid number.\n");
+
+while (getchar() != '\n')
+;
+}
+
+/* Get second number */
+while (1)
+{
+printf("Enter second number: ");
+
+check = scanf("%f", &num2);
+
+if (check != 1)
+{
+printf("Invalid input! Please enter a valid number.\n");
+
+while (getchar() != '\n')
+;
+continue;
+}
+
+if (choice == 4 && num2 == 0)
+{
+printf("Cannot divide by zero!\n");
+continue;
+}
+
+break;
+}
 
 switch (choice)
 {
 case 1:
-printf("Enter the first number: ");
-scanf("%f", &num1);
-
-do
-{
-printf("Enter the second number: ");
-scanf("%f", &num2);
-
-if (num2 == 0)
-{
-printf("Invalid! Cannot divide by zero.\n");
-printf("Please enter a non-zero number.\n");
-}
-
-} while (num2 == 0);
-
-result = num1 / num2;
-
-printf("Result = %.2f / %.2f = %.2f\n",
-num1, num2, result);
+result = num1 + num2;
+printf("Result = %.2f\n", result);
 break;
 
 case 2:
-printf("Goodbye!\n");
+result = num1 - num2;
+printf("Result = %.2f\n", result);
 break;
 
-default:
-printf("Invalid menu choice. Please try again.\n");
-}
+case 3:
+result = num1 * num2;
+printf("Result = %.2f\n", result);
+break;
 
-} while (choice != 2);
+case 4:
+result = num1 / num2;
+printf("Result = %.2f\n", result);
+break;
+}
+}
 
 return (0);
 }
