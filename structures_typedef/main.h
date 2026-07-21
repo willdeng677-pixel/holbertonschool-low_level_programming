723d5f517cabadae1e93b7a91b4badcf448f95b0
willdeng677-pixel <willdeng677@gmail.com>
2026-07-21 14:06:31 +0000

    main.h

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -2,16 +2,18 @@
 #define DOG_H
 
 /**
- * struct dog - stores information about a dog
+ * struct dog - defines a dog
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
  */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
