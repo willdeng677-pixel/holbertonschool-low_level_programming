d9bc89d9fb51587d9c6ae5c44a7c5e84092921f4
willdeng677-pixel <willdeng677@gmail.com>
2026-07-21 02:33:48 +0000

    Simple calculation

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -3,130 +3,50 @@
 int main(void)
 {
 int choice;
-int A;
-int B;
-int result;
+float num1, num2, result;
 
-printf("==========================================\n");
-printf(" Welcome to Simple Calculation\n");
-printf("==========================================\n");
+do
+{
+printf("\n===== Simple Calculator =====\n");
+printf("1. Divide two numbers\n");
+printf("2. Exit\n");
+printf("Enter your choice: ");
+scanf("%d", &choice);
 
-for (;;)
+switch (choice)
 {
-printf("\nMenu\n");
-printf("1. Addition\n");
-printf("2. Subtraction\n");
-printf("3. Multiplication\n");
-printf("4. Division\n");
-printf("0. Quit\n");
+case 1:
+printf("Enter the first number: ");
+scanf("%f", &num1);
 
-printf("Please enter your choice of an integer from 1..4 or 0 to quit: ");
+do
+{
+printf("Enter the second number: ");
+scanf("%f", &num2);
 
-/* Checking for validity of the entry */
-if (scanf("%d", &choice) != 1)
-{
-printf("Invalid input! Please enter an integer from 1..4 and 0 to quit.\n");
-return (1);
-}
-if (choice == 0)
-{
-printf("Goodbye!\n");
-break;
-}
-else if (choice == 1)
-{
-printf("You have selected addition.\n");
-}
-else if (choice == 2)
-{
-printf("You have selected Subtraction.\n");
-}
-else if (choice == 3)
-{
-printf("You have selected Multiplication.\n");
-}
-else if (choice == 4)
-{
-printf("You have selected Division.\n");
-printf("Please enter an integer number value of A:\n");
-scanf("%d", &A);
-if (B == 0)
+if (num2 == 0)
 {
 printf("Invalid! Cannot divide by zero.\n");
-printf("Please try again.\n\n");
+printf("Please enter a non-zero number.\n");
 }
+
+} while (num2 == 0);
+
+result = num1 / num2;
+
+printf("Result = %.2f / %.2f = %.2f\n",
+num1, num2, result);
+break;
+
+case 2:
+printf("Goodbye!\n");
+break;
+
+default:
+printf("Invalid menu choice. Please try again.\n");
 }
-else 
-{
-printf("Invalid menu option! Please enter an integer from 1...4 or 0 to quit.\n");
-}
-}
-/*============Addition=======================*/
-/* ask user to input the value of A */
-printf("Please enter an integer number value of A:\n");
-scanf("%d", &A);
 
-/* ask user to input the value of B */
-printf("Please enter an integer number value of B:\n");
-scanf("%d", &B);
-
-/* Adding up A and B values*/
-result = A+B;
-
-/*Printing the added up A value and B value*/
-printf("#########################################\n");
-printf("The sum of %d and %d is %d:\n", A, B, result);
-printf("#########################################\n");
-/*=============Subtraction=================*/
-/* ask user to input the value of A */
-printf("Please enter an integer number value of A:\n");
-scanf("%d", &A);
-
-/* ask user to input the value of B */
-printf("Please enter an integer number value of B:\n");
-scanf("%d", &B);
-
-/* Subtracting A and B values*/
-result = A-B;
-
-/*Printing the result of subtracting A value from B value*/
-printf("#########################################\n");
-printf("The Subtraction of %d and %d is %d:\n", A, B, result);
-printf("#########################################\n");
-
-/*============Multiplication=======================*/
-/* ask user to input the value of A */
-printf("Please enter an integer number value of A:\n");
-scanf("%d", &A);
-
-/* ask user to input the value of B */
-printf("Please enter an integer number value of B:\n");
-scanf("%d", &B);
-
-/* Multiplying A and B values*/
-result = A * B;
-
-/*Printing the Multiplication A value and B value*/
-printf("#########################################\n");
-printf("The Multiplication of %d and %d is %d:\n", A, B, result);
-printf("#########################################\n");
-
-/*============Division=======================*/
-/* ask user to input the value of A */
-printf("Please enter an integer number value of A:\n");
-scanf("%d", &A);
-
-/* ask user to input the value of B */
-printf("Please enter an integer number value of B:\n");
-scanf("%d", &B);
-
-/* Multiplying A and B values*/
-result = A / B;
-
-/*Printing the division A value and B value*/
-printf("#########################################\n");
-printf("The Division of %d and %d is %d:\n", A, B, result);
-printf("#########################################\n");
+} while (choice != 2);
 
 return (0);
 }
