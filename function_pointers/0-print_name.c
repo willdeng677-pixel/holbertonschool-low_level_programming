07c73e14629f4a97830be77fba29dd063f54ee96
willdeng677-pixel <willdeng77@gmail.com>
2026-07-28 12:55:29 +0000

    Function that print name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,26 +1,52 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
-#include <stdio.h>
-
-void print_normal(char *name)
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+void print_name_as_is(char *name)
 {
-    printf("%s\n", name);
+    printf("Hello, my name is %s\n", name);
 }
 
-void print_upper(char *name)
+/**
+ * print_name_uppercase - print a name in uppercase
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+void print_name_uppercase(char *name)
 {
-    while (*name)
+    unsigned int i;
+
+    printf("Hello, my uppercase name is ");
+    i = 0;
+    while (name[i])
     {
-        putchar(*name >= 'a' && *name <= 'z' ? *name - 32 : *name);
-        name++;
+        if (name[i] >= 'a' && name[i] <= 'z')
+        {
+            putchar(name[i] + 'A' - 'a');
+        }
+        else
+        {
+            putchar(name[i]);
+        }
+        i++;
     }
-    putchar('\n');
 }
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    print_name("William", print_normal);
-    print_name("William", print_upper);
-
+    print_name("Bob", print_name_as_is);
+    print_name("Bob Dylan", print_name_uppercase);
+    printf("\n");
     return (0);
 }
