8c779002a45f2a73deece1abbc92e136b2eb8a4e
willdeng677-pixel <willdeng77@gmail.com>
2026-07-28 16:07:22 +0000

    3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,33 +11,33 @@
  */
 int main(int argc, char *argv[])
 {
-	int a, b;
-	int (*operation)(int, int);
+int a, b;
+int (*operation)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
 
-	operation = get_op_func(argv[2]);
+operation = get_op_func(argv[2]);
 
-	if (operation == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
+if (operation == NULL)
+{
+printf("Error\n");
+exit(99);
+}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+a = atoi(argv[1]);
+b = atoi(argv[3]);
 
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
+if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 
-	printf("%d\n", operation(a, b));
+printf("%d\n", operation(a, b));
 
-	return (0);
+return (0);
 }
