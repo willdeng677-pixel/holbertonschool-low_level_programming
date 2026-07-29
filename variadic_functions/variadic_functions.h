b58f48c8af74c7fd7d9445ab49f7b0024372c228
willdeng677-pixel <willdeng677@gmail.com>
2026-07-29 13:13:56 +0000

    variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -12,6 +12,18 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+ * struct printer - matches a format specifier to a print function
+ * @type: format character
+ * @print: function to print the argument
+ */
+typedef struct printer
+{
+	char type;
+	void (*print)(va_list *, char *);
+} printer_t;
+
 void print_all(const char * const format, ...);
 
 #endif
