1d958e2f405549181c0a89cb590f62c1ba6a0a34
willdeng677-pixel <willdeng677@gmail.com>
2026-07-31 15:30:48 +0000

    Function that add positive numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * is_digit - checks if a string contains only digits
+ * @str: string to check
+ *
+ * Return: 1 if all characters are digits, 0 otherwise
+ */
+int is_digit(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+		{
+			return (0);
+		}
+	}
+
+	return (1);
+}
+
+/**
+ * main - adds positive numbers
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!is_digit(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
