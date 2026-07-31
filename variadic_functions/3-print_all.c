277d59198ad352f40577349bda829c0c0f5ba660
willdeng677-pixel <willdeng677@gmail.com>
2026-07-31 11:40:03 +0000

    Function that print everything

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,41 +1,83 @@
 #include <stdio.h>
-#include "variadic_functions.h"
+#include <stdarg.h>
+#include "print_all.h"
 
 /**
- * print_char - prints a character
+ * print_char - prints a char
  */
-void print_char(va_list *args, char *separator)
+void print_char(va_list *ap)
 {
-	printf("%s%c", separator, va_arg(*args, int));
+	printf("%c", va_arg(*ap, int));
 }
 
 /**
- * print_int - prints an integer
+ * print_int - prints an int
  */
-void print_int(va_list *args, char *separator)
+void print_int(va_list *ap)
 {
-	printf("%s%d", separator, va_arg(*args, int));
+	printf("%d", va_arg(*ap, int));
 }
 
 /**
  * print_float - prints a float
  */
-void print_float(va_list *args, char *separator)
+void print_float(va_list *ap)
 {
-	printf("%s%f", separator, va_arg(*args, double));
+	printf("%f", va_arg(*ap, double));
 }
 
 /**
  * print_string - prints a string
  */
-void print_string(va_list *args, char *separator)
+void print_string(va_list *ap)
 {
 	char *str;
 
-	str = va_arg(*args, char *);
+	str = va_arg(*ap, char *);
 
-	if (!str)
+	if (str == NULL)
 		str = "(nil)";
 
-	printf("%s%s", separator, str);
+	printf("%s", str);
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of argument types
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0, j;
+	char *sep = "";
+	va_list ap;
+
+	printer_t p[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{0, NULL}
+	};
+
+	va_start(ap, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+
+		while (p[j].symbol)
+		{
+			if (format[i] == p[j].symbol)
+			{
+				printf("%s", sep);
+				p[j].func(&ap);
+				sep = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+
+	va_end(ap);
+	printf("\n");
 }
