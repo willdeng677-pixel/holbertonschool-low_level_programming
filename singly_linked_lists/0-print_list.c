1023708ff2fb9ef44e2c61817974020930a7ebb8
willdeng677-pixel <willdeng677@gmail.com>
2026-08-12 15:07:52 +0000

    Functionthat prints all elements of a list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 #include "lists.h"
 
-typedef struct list_s
-{
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
-
 /**
  * print_list - prints all elements of a list_t list
  * @h: pointer to the head of the list
