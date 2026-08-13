e87766fbbba2cc5939356c645606cde56e9b901b
willdeng677-pixel <willdeng677@gmail.com>
2026-08-13 10:44:49 +0000

    Function that adds an element to the the hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,6 +3,27 @@
 #include <string.h>
 
 /**
+ * update_value - updates the value of an existing node
+ * @node: node to update
+ * @value: new value
+ *
+ * Return: 1 if successful, 0 otherwise
+ */
+int update_value(hash_node_t *node, const char *value)
+{
+	char *new_value;
+
+	new_value = strdup(value);
+	if (new_value == NULL)
+		return (0);
+
+	free(node->value);
+	node->value = new_value;
+
+	return (1);
+}
+
+/**
  * hash_table_set - adds or updates an element in a hash table
  * @ht: hash table
  * @key: key
@@ -13,8 +34,8 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node;
 	hash_node_t *current;
+	hash_node_t *new_node;
 
 	if (ht == NULL || key == NULL || key[0] == '\0')
 		return (0);
@@ -25,18 +46,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
-		{
-			char *new_value;
-
-			new_value = strdup(value);
-			if (new_value == NULL)
-				return (0);
-
-			free(current->value);
-			current->value = new_value;
-
-			return (1);
-		}
+			return (update_value(current, value));
 
 		current = current->next;
 	}
