2fba56c89a77968b3feab12645426a8556d9c51f
willdeng677-pixel <willdeng677@gmail.com>
2026-08-13 10:39:48 +0000

    Function that adds an element to the the hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -30,7 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 			new_value = strdup(value);
 			if (new_value == NULL)
-			return (0);
+				return (0);
 
 			free(current->value);
 			current->value = new_value;
