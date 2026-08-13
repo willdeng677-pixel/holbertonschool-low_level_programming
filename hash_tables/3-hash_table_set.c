dc015b6f0fc5fba67ed4304db5ec22e4b13e0966
willdeng677-pixel <willdeng677@gmail.com>
2026-08-13 10:48:27 +0000

    Function that adds an element to the the hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -24,6 +24,41 @@ int update_value(hash_node_t *node, const char *value)
 }
 
 /**
+ * create_node - creates a new hash node
+ * @key: key of the node
+ * @value: value of the node
+ *
+ * Return: pointer to new node, or NULL if it fails
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+
+	return (new_node);
+}
+
+/**
  * hash_table_set - adds or updates an element in a hash table
  * @ht: hash table
  * @key: key
@@ -51,24 +86,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		current = current->next;
 	}
 
-	new_node = malloc(sizeof(hash_node_t));
+	new_node = create_node(key, value);
 	if (new_node == NULL)
 		return (0);
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
 
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
