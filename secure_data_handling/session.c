22e06b5da74f1e442178b9b002d76c5e610d9014
willdeng677-pixel <willdeng677@gmail.com>
2026-08-14 00:41:00 +0000

    Function that creates session

---
+++ secure_data_handling/session.c
@@ -0,0 +1,86 @@
+#include <stdlib.h>
+#include <string.h>
+#include "session.h"
+
+session_t *session_create(const char *id, unsigned int uid,
+		const unsigned char *data, size_t data_len)
+{
+	session_t *s;
+
+	if (id == NULL)
+		return NULL;
+
+	if (data_len > 0 && data == NULL)
+		return NULL;
+
+	s = malloc(sizeof(*s));
+	if (s == NULL)
+		return NULL;
+
+	s->id = strdup(id);
+	if (s->id == NULL)
+	{
+		free(s);
+		return NULL;
+	}
+
+	s->uid = uid;
+	s->data = NULL;
+	s->data_len = 0;
+
+	if (data_len > 0)
+	{
+		s->data = malloc(data_len);
+		if (s->data == NULL)
+		{
+			free(s->id);
+			free(s);
+			return NULL;
+		}
+
+		memcpy(s->data, data, data_len);
+		s->data_len = data_len;
+	}
+
+	return s;
+}
+
+int session_set_data(session_t *s, const unsigned char *data,
+		size_t data_len)
+{
+	unsigned char *tmp;
+
+	if (s == NULL)
+		return 0;
+
+	if (data_len == 0)
+	{
+		free(s->data);
+		s->data = NULL;
+		s->data_len = 0;
+		return 1;
+	}
+
+	if (data == NULL)
+		return 0;
+
+	tmp = realloc(s->data, data_len);
+	if (tmp == NULL)
+		return 0;
+
+	s->data = tmp;
+	memcpy(s->data, data, data_len);
+	s->data_len = data_len;
+
+	return 1;
+}
+
+void session_destroy(session_t *s)
+{
+	if (s == NULL)
+		return;
+
+	free(s->id);
+	free(s->data);
+	free(s);
+}
