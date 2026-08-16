97fd1fab92d5ba6a5c612a88ed092422b3076e53
willdeng677-pixel <willdeng677@gmail.com>
2026-08-16 14:34:59 +0000

    Function that create session

--- secure_data_handling/session.c
+++ secure_data_handling/session.c
@@ -2,27 +2,38 @@
 #include <string.h>
 #include "session.h"
 
+/**
+ * session_create - creates a new session
+ * @id: session ID
+ * @uid: user ID
+ * @data: session data
+ * @data_len: length of session data
+ *
+ * Return: pointer to the new session, or NULL if it fails
+ */
 session_t *session_create(const char *id, unsigned int uid,
-		const unsigned char *data, size_t data_len)
+			  const unsigned char *data, size_t data_len)
 {
 	session_t *s;
+	size_t id_len;
 
 	if (id == NULL)
-		return (NULL);
-
-	if (data_len > 0 && data == NULL)
 		return (NULL);
 
 	s = malloc(sizeof(*s));
 	if (s == NULL)
 		return (NULL);
 
-	s->id = strdup(id);
+	id_len = strlen(id);
+
+	s->id = malloc(id_len + 1);
 	if (s->id == NULL)
 	{
 		free(s);
 		return (NULL);
 	}
+
+	memcpy(s->id, id, id_len + 1);
 
 	s->uid = uid;
 	s->data = NULL;
@@ -30,6 +41,13 @@ session_t *session_create(const char *id, unsigned int uid,
 
 	if (data_len > 0)
 	{
+		if (data == NULL)
+		{
+			free(s->id);
+			free(s);
+			return (NULL);
+		}
+
 		s->data = malloc(data_len);
 		if (s->data == NULL)
 		{
@@ -45,8 +63,16 @@ session_t *session_create(const char *id, unsigned int uid,
 	return (s);
 }
 
+/**
+ * session_set_data - replaces session data
+ * @s: session
+ * @data: new data
+ * @data_len: length of new data
+ *
+ * Return: 1 if successful, 0 otherwise
+ */
 int session_set_data(session_t *s, const unsigned char *data,
-		size_t data_len)
+		     size_t data_len)
 {
 	unsigned char *tmp;
 
@@ -75,12 +101,18 @@ int session_set_data(session_t *s, const unsigned char *data,
 	return (1);
 }
 
+/**
+ * session_destroy - destroys a session
+ * @s: session to destroy
+ *
+ * Return: nothing
+ */
 void session_destroy(session_t *s)
 {
 	if (s == NULL)
 		return;
 
-	free(s->data);
 	free(s->id);
+	free(s->data);
 	free(s);
 }
