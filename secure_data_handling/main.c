cb963436e28511f502209cc39368f52e9a96f598
willdeng677-pixel <willdeng677@gmail.com>
2026-08-16 15:34:18 +0000

    main.c

--- secure_data_handling/main.c
+++ secure_data_handling/main.c
@@ -1,34 +1,75 @@
 #include <stdio.h>
 #include <string.h>
 #include "session.h"
+#include "store.h"
 
 int main(void)
 {
-	session_t *session;
-	const unsigned char data[] = "hello";
-	const unsigned char new_data[] = "updated";
+	store_t store;
+	session_t *s1;
+	session_t *removed;
+	const unsigned char data[] = "test";
 
-	session = session_create("session1", 1001,
-			data, strlen((const char *)data));
+	store_init(&store);
 
-	if (session == NULL)
+	/* Create a session */
+	s1 = session_create("session1", 1001, data, strlen((const char *)data));
+	if (s1 == NULL)
 	{
-		printf("session_create failed\n");
+		printf("FAIL: session_create\n");
 		return (1);
 	}
 
-	printf("Session created: %s\n", session->id);
+	printf("Session created: %s\n", s1->id);
 
-	if (!session_set_data(session, new_data,
-			strlen((const char *)new_data)))
+	/* Add session to store */
+	if (!store_add(&store, s1))
 	{
-		session_destroy(session);
+		printf("FAIL: store_add\n");
+		session_destroy(s1);
+		return (1);
+	}
+
+	/* Get session */
+	if (store_get(&store, "session1") == NULL)
+	{
+		printf("FAIL: store_get\n");
+		store_destroy(&store);
+		return (1);
+	}
+
+	/* Update session data */
+	if (!session_set_data(s1, data, strlen((const char *)data)))
+	{
+		printf("FAIL: session_set_data\n");
+		store_destroy(&store);
 		return (1);
 	}
 
 	printf("Session data updated\n");
 
-	session_destroy(session);
+	/*
+	 * Delete the session.
+	 * Ownership is transferred to removed.
+	 */
+	removed = NULL;
+
+	if (!store_delete(&store, "session1", &removed))
+	{
+		printf("FAIL: store_delete\n");
+		store_destroy(&store);
+		return (1);
+	}
+
+	/*
+	 * The store no longer owns removed.
+	 * The caller must destroy it.
+	 */
+	if (removed != NULL)
+		session_destroy(removed);
+
+	/* Destroy any remaining sessions/nodes */
+	store_destroy(&store);
 
 	return (0);
 }
